38d9eb7b1e420303538cc27590de67932d72ae37
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2003-07-05 06:05:34 +0000

    *** empty log message ***

--- trunk/irate/irate/swt/AccountDialog.java
+++ trunk/irate/irate/swt/AccountDialog.java
@@ -19,74 +19,74 @@ import irate.common.*;
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class AccountDialog {
-	boolean done = false;
-	
-	public AccountDialog(Display display, TrackDatabase trackDatabase)
-	{
-		final Shell shell = new Shell(display);
-		shell.addShellListener(new ShellAdapter()
-		{
-		  public void shellClosed(ShellEvent e){
-			done=true;
-		  }
-		});
+  boolean done = false;
+  
+  public AccountDialog(Display display, TrackDatabase trackDatabase)
+  {
+    final Shell shell = new Shell(display);
+    shell.addShellListener(new ShellAdapter()
+    {
+      public void shellClosed(ShellEvent e){
+      done=true;
+      }
+    });
 
-		shell.setText("Account Settings");
-		GridLayout layout = new GridLayout(2, false);
-		shell.setLayout(layout);
-		
-		new Label(shell, SWT.NONE).setText("User");
-		final Text txtUser = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		txtUser.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL));;
-		txtUser.setText(trackDatabase.getUserName());
-		
-		new Label(shell, SWT.NONE).setText("Password");
-		final Text txtPassword = new Text(shell,SWT.SINGLE | SWT.BORDER);
-		txtPassword.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));;
-		txtPassword.setText(trackDatabase.getPassword());
-		
-		new Label(shell, SWT.NONE).setText("Host");
-		final Text txtHost = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		txtHost.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));
-		txtHost.setText(trackDatabase.getHost());
-		
-		new Label(shell, SWT.NONE).setText("Port");
-		final Text txtPort = new Text(shell,SWT.SINGLE | SWT.BORDER);
-		txtPort.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));
-		txtPort.setText(""+trackDatabase.getPort());
-		
-		Button btnSave = new Button(shell, SWT.NONE);
-		btnSave.setText("Save");
-		
-		final TrackDatabase finTrackDatabase  = trackDatabase;
-		btnSave.addSelectionListener(new SelectionAdapter(){
-			  public void widgetSelected(SelectionEvent e){
-			  	finTrackDatabase.setUserName(txtUser.getText());
-			  	finTrackDatabase.setPassword(txtPassword.getText());
-			  	finTrackDatabase.setHost(txtHost.getText());
-			  	finTrackDatabase.setPort(Integer.parseInt(txtPort.getText()));
-			  	done = true;
-			  }
-			});    
-		
-		Button btnCancel = new Button(shell, SWT.NONE);
-		btnCancel.setText("Cancel");
-		btnCancel.addSelectionListener(new SelectionAdapter(){
-		  public void widgetSelected(SelectionEvent e){
-			done = true;
-			if(finTrackDatabase.getUserName().length() == 0 || finTrackDatabase.getPassword().length() == 0)
-				System.exit(0);
-		  }
-		});    
-		
-		shell.pack();    
-		shell.open();
-		while (!done) {
-			if (!display.readAndDispatch()) display.sleep();
-		}	
-		shell.close();
-		shell.dispose();
-	}
-	
-	
+    shell.setText("Account Settings");
+    GridLayout layout = new GridLayout(2, false);
+    shell.setLayout(layout);
+    
+    new Label(shell, SWT.NONE).setText("User");
+    final Text txtUser = new Text(shell, SWT.SINGLE | SWT.BORDER);
+    txtUser.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL));;
+    txtUser.setText(trackDatabase.getUserName());
+    
+    new Label(shell, SWT.NONE).setText("Password");
+    final Text txtPassword = new Text(shell,SWT.SINGLE | SWT.BORDER);
+    txtPassword.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));;
+    txtPassword.setText(trackDatabase.getPassword());
+    
+    new Label(shell, SWT.NONE).setText("Host");
+    final Text txtHost = new Text(shell, SWT.SINGLE | SWT.BORDER);
+    txtHost.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));
+    txtHost.setText(trackDatabase.getHost());
+    
+    new Label(shell, SWT.NONE).setText("Port");
+    final Text txtPort = new Text(shell,SWT.SINGLE | SWT.BORDER);
+    txtPort.setLayoutData(new GridData(GridData.FILL_HORIZONTAL| GridData.FILL_VERTICAL));
+    txtPort.setText(""+trackDatabase.getPort());
+    
+    Button btnSave = new Button(shell, SWT.NONE);
+    btnSave.setText("Save");
+    
+    final TrackDatabase finTrackDatabase  = trackDatabase;
+    btnSave.addSelectionListener(new SelectionAdapter(){
+        public void widgetSelected(SelectionEvent e){
+          finTrackDatabase.setUserName(txtUser.getText());
+          finTrackDatabase.setPassword(txtPassword.getText());
+          finTrackDatabase.setHost(txtHost.getText());
+          finTrackDatabase.setPort(Integer.parseInt(txtPort.getText()));
+          done = true;
+        }
+      });    
+    
+    Button btnCancel = new Button(shell, SWT.NONE);
+    btnCancel.setText("Cancel");
+    btnCancel.addSelectionListener(new SelectionAdapter(){
+      public void widgetSelected(SelectionEvent e){
+      done = true;
+      if(finTrackDatabase.getUserName().length() == 0 || finTrackDatabase.getPassword().length() == 0)
+        System.exit(0);
+      }
+    });    
+    
+    shell.pack();    
+    shell.open();
+    while (!done) {
+      if (!display.readAndDispatch()) display.sleep();
+    } 
+    shell.close();
+    shell.dispose();
+  }
+  
+  
 }
