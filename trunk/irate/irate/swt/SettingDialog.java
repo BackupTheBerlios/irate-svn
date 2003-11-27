93fe89fa8302e73c14e24c58c883c085062364ba
eythian <eythian@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2003-11-27 04:18:11 +0000

    Now text entered in the browser box will stick.

--- trunk/irate/irate/swt/SettingDialog.java
+++ trunk/irate/irate/swt/SettingDialog.java
@@ -76,11 +76,11 @@ public class SettingDialog
 		//Pluginpage
     TabItem tabItem = new TabItem(tabs, SWT.NONE);
     tabItem.setText("Plugins");
-		tabItem.setControl(createPluginPage(tabs));
+    tabItem.setControl(createPluginPage(tabs));
     tabItem = new TabItem(tabs, SWT.NONE);
     tabItem.setText("Browser");
-		tabItem.setControl(createBrowserPage(tabs));
-		tabs.pack();
+    tabItem.setControl(createBrowserPage(tabs));
+    tabs.pack();
 
     Button ok = new Button(shell, SWT.NONE);
     ok.setText("Close");
@@ -88,31 +88,31 @@ public class SettingDialog
     //gd.horizontalSpan = 2;
     ok.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
     ok.addSelectionListener(new SelectionAdapter(){
-      public void widgetSelected(SelectionEvent e){
-        try {
-          Preferences.savePreferenceToFile("browser", browser);
-        } catch (IOException ioe) {
-          ioe.printStackTrace();
+        public void widgetSelected(SelectionEvent e){
+          try {
+            Preferences.savePreferenceToFile("browser", browser);
+          } catch (IOException ioe) {
+            ioe.printStackTrace();
+            shell.close();
+          }
           shell.close();
         }
-        shell.close();
-      }
-    });
-		
+      });
+    
     shell.pack();    
   }
   
-	private Composite createPluginPage(Composite parent) {
-		Composite comp = new Composite(parent, SWT.NONE);
+  private Composite createPluginPage(Composite parent) {
+    Composite comp = new Composite(parent, SWT.NONE);
     GridLayout layout = new GridLayout(2, false);
     comp.setLayout(layout);
-
+    
     Label heading = new Label(comp, SWT.NONE);
     heading.setText("Select the plugins you wish to enable");
     GridData gd = new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING);
     gd.horizontalSpan = 2;
     heading.setLayoutData(gd);
-
+    
     java.util.List plugins = pluginManager.getPlugins();
     for (int i = 0; i < plugins.size(); i++) {
       final Plugin plugin = (Plugin) plugins.get(i);
@@ -120,28 +120,28 @@ public class SettingDialog
       checkbox.setText(plugin.getDescription());
       checkbox.setSelection(plugin.isAttached());
       checkbox.addSelectionListener(new SelectionAdapter(){
-				public void widgetSelected(SelectionEvent e){
-					if (checkbox.getSelection())
-						plugin.attach(pluginManager.getApp());
-								else
-						plugin.detach();
-				}
-      });
+          public void widgetSelected(SelectionEvent e){
+            if (checkbox.getSelection())
+              plugin.attach(pluginManager.getApp());
+            else
+              plugin.detach();
+          }
+        });
       Button configure = new Button(comp, SWT.NONE);
       configure.setText("Configure");
       configure.addSelectionListener(new SelectionAdapter(){
         public void widgetSelected(SelectionEvent e){
           app.getUIFactory().lookup(plugin, PluginUIFactory.CONFIGURATOR);
         }
-			});
-		}
-
-		return comp;
-	}
+        });
+    }
+    
+    return comp;
+  }
 
   
-	private Composite createBrowserPage(Composite parent) {
-		Composite comp = new Composite(parent, SWT.NONE);
+  private Composite createBrowserPage(Composite parent) {
+    Composite comp = new Composite(parent, SWT.NONE);
     GridLayout layout = new GridLayout(1, false);
     comp.setLayout(layout);
     new Label(comp, SWT.NONE).setText("Browser Executable:");
@@ -149,18 +149,23 @@ public class SettingDialog
     final Text browserText = new Text(comp, SWT.NONE);
     browserText.setText(browser);
     browserText.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+    browserText.addModifyListener(new ModifyListener(){
+        public void modifyText(ModifyEvent e) {
+          browser = browserText.getText();
+        }
+      });
     
     Button browseButton = new Button(comp, SWT.NONE);
     browseButton.setText("Browse ...");
     
     browseButton.addSelectionListener(new SelectionAdapter(){
-      public void widgetSelected(SelectionEvent e){
-        FileDialog fd = new FileDialog(shell, SWT.OPEN);
-        fd.open();
-        browserText.setText(fd.getFilterPath() + "/" + fd.getFileName());
-        browser = browserText.getText();
-      }
-    });
+        public void widgetSelected(SelectionEvent e){
+          FileDialog fd = new FileDialog(shell, SWT.OPEN);
+          fd.open();
+          browserText.setText(fd.getFilterPath() + "/" + fd.getFileName());
+          browser = browserText.getText();
+        }
+      });
     
     
     return comp;
