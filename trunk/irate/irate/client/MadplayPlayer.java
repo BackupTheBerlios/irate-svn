abf7d332cc061e77bd2b43df1c12ccd07d4f3db5
emh_mark3 <emh_mark3@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-02-18 06:54:10 +0000

    Added positionUpdated capability to MadPlayPlayer. This is done by receiving position update every second on the standard output.

--- trunk/irate/irate/client/MadplayPlayer.java
+++ trunk/irate/irate/client/MadplayPlayer.java
@@ -3,8 +3,16 @@
 package irate.client;
 
 import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.*;
 
 public class MadplayPlayer extends ExternalPlayer {
+
+  private boolean bAcquireNewTrackLength = true;
+  private int trackLength = 0;
 
   public MadplayPlayer()
     throws FileNotFoundException
@@ -35,4 +43,116 @@ public class MadplayPlayer extends ExternalPlayer {
   {
     return new String[] { "--amplify", Integer.toString(getVolume()) };
   }
+  
+  public String[] formatOtherArguments() 
+  {
+    // We make the player output the remaining time in order to obtain the lenght of the track at the beginning
+    return new String[] { "-v", "--display-time=remaining" };
+  }
+  
+  /*
+   * New file is being played -- reset track lenght
+   */
+  public void play(File file) throws PlayerException {
+    bAcquireNewTrackLength = true;
+    super.play(file);
+  }
+  
+  /*
+   * Hacked route() routine used to grab player output from stdout
+   */
+   public void route(final InputStream is, final OutputStream os, String title) {
+    new Thread(title) {
+      public void run() {
+        String currentLine;
+        
+        try {
+          BufferedReader in = new BufferedReader(new InputStreamReader(is));
+          BufferedWriter out = new BufferedWriter(new OutputStreamWriter(os));
+          
+          while (true) {
+            currentLine = in.readLine();
+            if (currentLine != null) {
+              if (currentLine.charAt(0) != '-') {
+                out.write(currentLine + '\n');
+                out.flush();
+              }
+              else {
+                processPlayerTrackOutput(currentLine);
+                out.flush();
+              }
+            } 
+          }
+        } 
+        catch (IOException ioe) {
+          //ioe.printStackTrace();
+        }
+      }
+    }.start();
+  }
+   
+   /**
+    * Analyses the player's output to obtain the current position in track. Sends
+    * notify to listeners when done.
+    */
+  public void processPlayerTrackOutput(String line) {
+    String[] splits;
+    String data;
+    int seconds, minutes, hours, time;
+    
+    try {
+      data = line.substring(1); // remove first char (negative sign)
+      //splits = data.split(" ");
+      //splits = splits[0].split(":");
+      splits = split(data, ' '); // isolate position indicator
+      splits = split(splits[0], ':'); // split in hours, minutes & seconds
+
+      hours = Integer.valueOf(splits[0]).intValue();
+      minutes = Integer.valueOf(splits[1]).intValue();
+      seconds = Integer.valueOf(splits[2]).intValue();
+      
+      time = hours * 3600 + minutes * 60 + seconds;
+      
+      if (bAcquireNewTrackLength) {
+        // if a new track started, use the first event we get from the player to calculate
+        // the lenght of the track
+        trackLength = time;
+        bAcquireNewTrackLength = false;
+      }
+      
+      notifyPosition(trackLength - time, trackLength);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Splits a string in multiple substrings. Implemented here instead of using String.split() in order
+   * to remain compatible with JDK 1.3. Maximum of 4 string divisions.
+   */
+  private String[] split(String data, char divisor) {
+    String[] temp = new String[4];
+    int currentSubString = 0;
+    int lastpos = 0;
+
+    while (firstIndexOf(data, divisor, lastpos) != -1 && currentSubString != temp.length - 1) {
+      temp[currentSubString] = data.substring(lastpos, firstIndexOf(data, divisor, lastpos));
+      lastpos = firstIndexOf(data, divisor, lastpos) + 1;
+      currentSubString++;
+    }
+    
+    temp[currentSubString] = data.substring(lastpos);
+
+    return temp;
+  }
+  
+  private int firstIndexOf(String data, char c, int pos) {
+    for (int i = pos; i < data.length(); i++)
+      if (data.charAt(i) == c)
+        return i;
+    
+    return -1;
+  }
+   
 }
