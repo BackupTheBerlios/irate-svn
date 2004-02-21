95c2714a1e13c64606b6b92516e941fe69d3a93f
emh_mark3 <emh_mark3@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-02-21 04:43:49 +0000

    Added ability to get resources as files -- resource will be written to a temporary file that will be deleted upon client exit.

--- trunk/irate/irate/resources/BaseResources.java
+++ trunk/irate/irate/resources/BaseResources.java
@@ -9,6 +9,14 @@ import java.net.URL;
 import java.util.Hashtable;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.Vector;
+import java.io.File;
+import java.util.Enumeration;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 /**
  * @author Anthony Jones
@@ -45,6 +53,66 @@ public class BaseResources {
     catch (MissingResourceException e) {
       return '!' + key + '!';
     }
+  }
+  
+  /**
+   * Function used to extract a ressource into a temp folder and return the path of the
+   * file. A vector can be passed to store filenames in -- those filenames refer to ressources
+   * that have already been extracted (and can be reused for extra efficiency).
+   */
+  public static File getResourceAsFile(String name, Vector tempFiles) throws IOException {
+    File myTempFile;
+    String[] myStrings;   // Used in tempFiles: first string is the ressource name, second is the file path
+    if(getResource(name) != null) {
+      
+      // Search tempFile list to see if ressource already extracted
+      if (tempFiles != null) {
+        for (Enumeration e = tempFiles.elements(); e.hasMoreElements();) {
+          myStrings = (String[])(e.nextElement());
+          if (name.equals(myStrings[0]))
+            return new File(myStrings[1]);
+        }
+      }
+      
+      // Create temp file
+      myTempFile = File.createTempFile("irate", name);
+      myTempFile.deleteOnExit();
+      
+      // Open ressource
+      URL url = getResource(name);
+      URLConnection urlc = url.openConnection();
+      InputStream is = url.openStream();
+      BufferedReader in = new BufferedReader(new InputStreamReader(is));
+      BufferedWriter out = new BufferedWriter(new FileWriter(myTempFile));
+      
+      // Copy resource into temp file
+      int onechar;
+      while ((onechar = in.read()) != -1)
+        out.write(onechar);
+      
+      // Close streams
+      in.close();
+      out.close();
+      
+      // Add file to filelist if required
+      if (tempFiles != null) {
+        myStrings = new String[] {name, myTempFile.getPath() };
+        tempFiles.add(myStrings);
+      }
+      
+      return myTempFile;
+    }
+    else
+      throw new IOException();
+  }
+  
+  /**
+   * Get a ressource as a temporary file. A new file will be created each time
+   * this function is called, even if the same ressource is accessed twice. To prevent
+   * this, use getResourceAsFile(String, Vector) instead.
+   */
+  public static File getResourceAsFile(String name) throws IOException {
+    return getResourceAsFile(name, null);
   }
   
 }
