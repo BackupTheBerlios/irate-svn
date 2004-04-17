07e77e0bd690760b5218a7271c4ab4bcaaed61f5
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-04-17 11:41:25 +0000

    Added missing interface.

---
+++ trunk/irate/irate/swt/Skinable.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Apr 16, 2004
+ */
+package irate.swt;
+
+import org.eclipse.swt.graphics.ImageData;
+
+/**
+ * @author Anthony Jones
+ */
+public interface Skinable {
+  public void setTransparencyManager(TransparencyManager tm);
+  public void setText(String text);
+  public void setPressedText(String text);
+  public void setToolTipText(String text);
+  public void setImage(String key, ImageData imageData);
+  public void redraw();      
+}
