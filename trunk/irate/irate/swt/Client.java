e1ea5b0c350054a0fd118a13623547d9cbfc99c6
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-04-20 16:32:49 +0000

    Turned the border back on for the trackGroup. It doesn't seem to display quite right.

--- trunk/irate/irate/swt/Client.java
+++ trunk/irate/irate/swt/Client.java
@@ -707,7 +707,7 @@ public class Client extends AbstractClient {
   }
 
   public void createToolBar() {
-    trackGroup = new Composite(topPanel, SWT.FLAT );
+    trackGroup = new Composite(topPanel, SWT.BORDER);
     skinManager.addControl(trackGroup, "panel.track");
     trackGroup.setEnabled(false);
     GridData gridData = new GridData();
