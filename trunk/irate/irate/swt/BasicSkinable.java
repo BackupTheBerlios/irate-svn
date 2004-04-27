026d380db1cc560154faf7e4be83147fa24f5162
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-04-27 16:38:10 +0000

    Forgot the missing file.

---
+++ trunk/irate/irate/swt/BasicSkinable.java
@@ -0,0 +1,42 @@
+/*
+ * Created on Apr 27, 2004
+ */
+package irate.swt;
+
+import java.util.Hashtable;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * @author Anthony Jones
+ */
+public class BasicSkinable implements Skinable {
+
+  private Hashtable images = new Hashtable();
+  
+  public BasicSkinable(Display display) {
+  }
+  
+  public ImageData getImageData(String key) {
+    return (ImageData) images.get(key); 
+  }
+
+  public void setTransparencyManager(TransparencyManager tm) {
+  }
+
+  public void setText(String key, String text) {
+  }
+
+  public void setToolTipText(String key, String text) {
+  }
+
+  public void setImage(String key, ImageData imageData) {
+    images.put(key, imageData);    
+  }
+  
+  public void redraw() {
+  }
+
+}
