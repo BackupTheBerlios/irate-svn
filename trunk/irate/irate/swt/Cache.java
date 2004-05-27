bd908fe36460b0f0475594c60642be6d95472dc4
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-05-27 14:22:40 +0000

    Removed the cache because it doesn't work properly.

--- trunk/irate/irate/swt/Cache.java
+++ trunk/irate/irate/swt/Cache.java
@@ -12,101 +12,18 @@ import java.util.LinkedList;
  */
 public class Cache  {
 
-  private final String name;
-  private static final int expiryMillis = 5000;
-  private LinkedList list = new LinkedList();
-  private Object signal = new Object();
-  
+  // Disabled the cache because I've decided that it's a crap way to do things.
+  // When I get time I will speed up the image processing using a better 
+  // method
+
   public Cache(String name) {
-    this.name = name;
-    Thread t = new Thread() {
-      public void run() {
-        while (true) {
-          try {
-            synchronized (signal) {
-              signal.wait();
-            }
-            while (true) { 
-              synchronized (Cache.this) {
-                if (list.size() == 0)
-                  break;
-              }
-              Thread.sleep(2000);
-              flush();
-            }
-          }
-          catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    };
-    t.start();
   }
   
   synchronized public void put(Object key, Object value) {
-    Pair pair = findPair(key);
-    if (pair == null) {
-      pair = new Pair();
-      pair.key = key;
-      pair.value = value;
-      pair.millis = System.currentTimeMillis();
-      list.add(new SoftReference(pair));
-    }
-    else {
-      pair.value = value;
-    }
-    synchronized (signal) {
-      signal.notify();
-    }
   }
   
   synchronized public Object get(Object key) {
-    Pair pair = findPair(key);
-    if (pair == null)
-      return null;
-    return pair.value;
-  }
-  
-  private Pair findPair(Object key) {
-    for (Iterator itr = list.iterator(); itr.hasNext(); ) {
-      SoftReference ref = (SoftReference) itr.next();
-      Pair pair = (Pair) ref.get();
-      if (pair == null) 
-        itr.remove();
-      else
-        if (pair.key.equals(key)) {
-          pair.millis = System.currentTimeMillis();
-          return pair;
-        }
-    }    
     return null;
-  }
-  
-  synchronized private void flush() {
-//    boolean removed = false;
-    long millis = System.currentTimeMillis();
-    for (Iterator itr = list.iterator(); itr.hasNext(); ) {
-      SoftReference ref = (SoftReference) itr.next();
-      Pair pair = (Pair) ref.get();
-      if (pair == null) { 
-        itr.remove();
-      }
-      else {        
-        if (millis - pair.millis > expiryMillis) {
-          itr.remove();
-//          removed = true;
-        }
-      }
-    }    
-//    if (removed)
-//      System.out.println("Packed " + name + " cache size " + list.size());
-  }  
-
-  class Pair {
-    Object key;
-    Object value;
-    long millis;
   }
 }
 
