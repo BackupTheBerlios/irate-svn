f3e12e89d4594d6f8dde885572d6f67807e62f85
vibber <vibber@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2004-09-02 18:07:44 +0000

    Remove bogus package specifier; Java refuses to load the class when running the jar.

--- trunk/irate/mac/MacLauncher.java
+++ trunk/irate/mac/MacLauncher.java
@@ -1,5 +1,3 @@
-package mac;
-
 // Quick ugly mac launcher; unzips the app bundle and executes it.
 // Java Web Start doesn't currently work directly with SWT apps on Mac OS X as
 // SWT needs a tweaked java launcher.
