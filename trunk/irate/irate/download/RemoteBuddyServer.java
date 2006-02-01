fad91c96335ebdb310a4c64ab6dd88bd29bf2be1
ajones <ajones@b6ba5dd1-bae8-0310-8909-a76a83df8ea7>
2006-02-01 05:45:02 +0000

    Buddy server updates

--- trunk/irate/irate/download/RemoteBuddyServer.java
+++ trunk/irate/irate/download/RemoteBuddyServer.java
@@ -11,10 +11,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
@@ -51,15 +53,13 @@ public class RemoteBuddyServer implements RemoteServer {
     client = new XmlRpcClient(url);
   }
 
-  public void contactServer(TrackDatabase trackDatabase)
-      throws DownloadException {
+  public void contactServer(TrackDatabase trackDatabase) throws DownloadException {
     try {
       // Vector args = new Vector();
       // Object response = (Object) client.execute("Session.ping", args);
 
       System.out.print("Logging in...");
-      String sessionId = login(trackDatabase.getUserName(), trackDatabase
-          .getPassword(), true);
+      String sessionId = login(trackDatabase.getUserName(), trackDatabase.getPassword(), true);
       System.out.println(" done");
 
       System.out.print("Sending ratings...");
@@ -67,19 +67,23 @@ public class RemoteBuddyServer implements RemoteServer {
       System.out.println(" done");
 
       System.out.print("Fetching list...");
-      String[] newTracks = fetchTracks(sessionId, trackDatabase.getTracks());
-      System.out.println(" done");
-      System.out.println("No of tracks: " + newTracks.length);
+      RatingPair[] ratingPairs = fetchTracks(sessionId);
+      System.out.println(ratingPairs.length + " tracks");
 
-      if (newTracks.length != 0) {
-        System.out.print("Fetching track details...");
+      if (ratingPairs.length != 0) {
+        String[] newTracks = findNewTracks(ratingPairs, getTrackMap(trackDatabase));
+        System.out.print("Fetching details for " + newTracks.length + " new tracks...");
         Track[] tracks = fetchTrackDetails(sessionId, newTracks);
         System.out.println(" done");
 
-        System.out.print("Updating track database...");
+        System.out.print("Merging tracks from the server...");
         updateTrackDatabase(trackDatabase, tracks);
         System.out.println(" done");
       }
+
+      System.out.print("Updating ratings...");
+      int noOfChanges = updateRatings(getTrackMap(trackDatabase), ratingPairs);
+      System.out.println(" " + noOfChanges + " changes - done");
 
       System.out.print("Logging out...");
       logout(sessionId);
@@ -96,8 +100,7 @@ public class RemoteBuddyServer implements RemoteServer {
   private Hashtable convertTrackToHashTable(Track track) {
     Hashtable hashtable = new Hashtable();
     hashtable.put("url", track.getURL().toString());
-    hashtable.put("rating", new Float(track.isRated() ? track.getRating()
-        : Float.NaN));
+    hashtable.put("rating", new Float(track.isRated() ? track.getRating() : Float.NaN));
     return hashtable;
   }
 
@@ -111,8 +114,7 @@ public class RemoteBuddyServer implements RemoteServer {
     return vector;
   }
 
-  public String login(String userName, String password, boolean create)
-      throws XmlRpcException, IOException {
+  public String login(String userName, String password, boolean create) throws XmlRpcException, IOException {
     Vector args = new Vector();
     args.add(userName);
     args.add(password);
@@ -127,8 +129,7 @@ public class RemoteBuddyServer implements RemoteServer {
     client.execute("Session.logout", args);
   }
 
-  private void setRatings(String sessionId, Track[] tracks)
-      throws XmlRpcException, IOException {
+  private void setRatings(String sessionId, Track[] tracks) throws XmlRpcException, IOException {
     Vector args = new Vector();
     args.add(sessionId);
     args.add(convertTracksToVector(tracks));
@@ -136,30 +137,46 @@ public class RemoteBuddyServer implements RemoteServer {
     client.execute("Rating.setTrackData", args);
   }
 
-  private String[] fetchTracks(String sessionId, Track[] tracks)
-      throws XmlRpcException, IOException {
+  private RatingPair[] fetchTracks(String sessionId) throws XmlRpcException, IOException {
     Vector args = new Vector();
     args.add(sessionId);
     Object response = (Object) client.execute("Rating.getTracks", args);
     Vector newTracks = (Vector) response;
 
-    Set trackIds = new HashSet();
-    for (int i = 0; i < tracks.length; i++)
-      trackIds.add(tracks[i].getId());
-
     List newTrackIds = new ArrayList();
     for (Iterator itr = newTracks.iterator(); itr.hasNext();) {
       Hashtable rating = (Hashtable) itr.next();
-      String id = (String) rating.get("trackId");
-      if (!trackIds.contains(id))
-        newTrackIds.add(id);
+      RatingPair ratingPair = new RatingPair();
+      ratingPair.trackId = (String) rating.get("trackId");
+      ratingPair.rating = ((Number) rating.get("rating")).floatValue();
+      newTrackIds.add(ratingPair);
     }
 
-    return (String[]) newTrackIds.toArray(new String[newTrackIds.size()]);
+    return (RatingPair[]) newTrackIds.toArray(new RatingPair[newTrackIds.size()]);
   }
 
-  private Track[] fetchTrackDetails(String sessionId, String[] trackIds)
-      throws XmlRpcException, IOException {
+  private Map getTrackMap(TrackDatabase trackDatabase) {
+    Track[] tracks = trackDatabase.getTracks();
+    Map map = new HashMap();
+    for (int i = 0; i < tracks.length; i++) {
+      Track track = tracks[i];
+      map.put(track.getId(), track);
+    }
+    return map;
+  }
+
+  private String[] findNewTracks(RatingPair[] ratingPairs, Map trackMap) {
+    List newTracks = new ArrayList();
+    for (int i = 0; i < ratingPairs.length; i++) {
+      RatingPair ratingPair = new RatingPair();
+      if (!trackMap.containsKey(ratingPair.trackId))
+        newTracks.add(ratingPair.trackId);
+    }
+    return (String[]) newTracks.toArray(new String[newTracks.size()]);
+  }
+
+  private Track[] fetchTrackDetails(String sessionId, String[] trackIds) throws XmlRpcException, IOException {
+
     Vector args = new Vector();
     args.add(sessionId);
     args.add(new Vector(Arrays.asList(trackIds)));
@@ -171,6 +188,7 @@ public class RemoteBuddyServer implements RemoteServer {
     List tracks = new ArrayList();
     for (Iterator itr = tracksDetails.iterator(); itr.hasNext();) {
       Hashtable trackDetails = (Hashtable) itr.next();
+
       String url = (String) trackDetails.get("url");
       String trackId = (String) trackDetails.get("trackId");
       String artist = (String) trackDetails.get("artist");
@@ -192,6 +210,21 @@ public class RemoteBuddyServer implements RemoteServer {
     return (Track[]) tracks.toArray(new Track[tracks.size()]);
   }
 
+  private int updateRatings(Map trackMap, RatingPair[] ratingPairs) {
+    int ratingsUpdated = 0;
+    List newTracks = new ArrayList();
+    for (int i = 0; i < ratingPairs.length; i++) {
+      RatingPair ratingPair = new RatingPair();
+      Track track = (Track) trackMap.get(ratingPair.trackId);
+      if (ratingPair.rating == track.getRating()) {
+        System.out.println("Was " + track.getRating() + " should be " + ratingPair.rating);
+        // track.setRating(ratingPair.rating);
+        ratingsUpdated++;
+      }
+    }
+    return ratingsUpdated;
+  }
+
   private void updateTrackDatabase(TrackDatabase trackDatabase, Track[] tracks) {
     int updated = 0;
     int added = 0;
@@ -207,5 +240,11 @@ public class RemoteBuddyServer implements RemoteServer {
       }
     }
     System.out.print("Added " + added + " Updated " + updated);
+  }
+
+  private static class RatingPair {
+    public String trackId;
+
+    public float rating;
   }
 }
